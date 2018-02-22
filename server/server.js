0dc1ecc62115df43910214f1b3c86d7c3637628f
Dylan <dylan@MacBook-Pro-de-Dylan.local>
2018-02-22 14:42:48 -0500

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {User} = require('./models/user');
 var {ObjectID} = require('mongodb');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -32,7 +33,7 @@ app.get('/todos', (req, res) => {
   });
 });
 
-//GET /todo/1234346
+//GET /todo/:id
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
@@ -52,8 +53,8 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
 });
 
 
