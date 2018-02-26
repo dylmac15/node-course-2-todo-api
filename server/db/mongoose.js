e7f8e8b8d6c54346750c67a37434c13248380672
Dylan <dylan@MacBook-Pro-de-Dylan.local>
2018-02-25 22:23:01 -0500

    Connection DB

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,26 +1,34 @@
 var mongoose = require('mongoose');
-var {MongoClient} = require('mongodb');
+//var {MongoClient} = require('mongodb');
 
 const MONGODB_URL = 'mongodb://dyl:1234@ds245548.mlab.com:45548/todo';
 
-mongoose.Promise = global.Promise;
-
 // connect to database
-mongoose.connect(process.env.MONGODB_URL,{
+	mongoose.connect(MONGODB_URL,{
 		//useMongoClient: true,
-	promiseLibrary: global.Promise
-});
+		promiseLibrary: global.Promise
+	});
 
-var db = mongoose.connection;
+	var db = mongoose.connection;
 
-// mongodb error
-db.on('error', console.error.bind(console, 'connection error:'));
-// mongodb connection open
-db.once('open', () => {
-	 console.log(`Connected to Mongo at: ${new Date()}`)
+	// mongodb error
+	db.on('error', console.error.bind(console, 'connection error:'));
+	// mongodb connection open
+	db.once('open', () => {
+	  console.log(`Connected to Mongo at: ${new Date()}`)
+	});
+
+
+/*
+mongoose.Promise = global.Promise;
+MongoClient.connect(MONGODB_URL || 'mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err) {
+    return console.log(err);
+  }
+  console.log('DB connection successful');
 });
+*/
 
 //mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/TodoApp');
-
 
 module.exports = {mongoose};
