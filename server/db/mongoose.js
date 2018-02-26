34f26d56455c11f756f272f29d0931724d8c7675
Dylan <dylan@MacBook-Pro-de-Dylan.local>
2018-02-25 21:40:50 -0500

    New DB connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,11 +4,20 @@ var {MongoClient} = require('mongodb');
 const MONGODB_URL = 'mongodb://dyl:1234@ds245548.mlab.com:45548/todo';
 
 mongoose.Promise = global.Promise;
-MongoClient.connect(MONGODB_URL || 'mongodb://localhost:27017/TodoApp', (err, db) => {
-  if(err) {
-    return console.log(err);
-  }
-  console.log('DB connection successful');
+
+// connect to database
+mongoose.connect(MONGODB_URL,{
+		//useMongoClient: true,
+	promiseLibrary: global.Promise
+});
+
+var db = mongoose.connection;
+
+// mongodb error
+db.on('error', console.error.bind(console, 'connection error:'));
+// mongodb connection open
+db.once('open', () => {
+	 console.log(Connected to Mongo at: ${new Date()})
 });
 
 //mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/TodoApp');
