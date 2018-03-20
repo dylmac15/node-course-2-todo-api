7c876f561fd9a632bb51752f84394ea209ce570f
Dylan <dylan@MacBook-Pro-de-Dylan.local>
2018-03-20 17:10:07 -0400

    DB setup

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 const MONGODB_URL = 'mongodb://dyl:1234@ds245548.mlab.com:45548/todo';
 
 // connect to database
-	mongoose.connect(process.env.MONGODB_URI,{
+	mongoose.connect(process.env.MONGODB_URI || MONGODB_URL,{
 		//useMongoClient: true,
 		promiseLibrary: global.Promise
 	});
