77c4cf94898695df5bc1eef7706137d7d14ce372
Dylan <dylan@MacBook-Pro-de-Dylan.local>
2018-03-26 14:59:41 -0400

    Modifications

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
     email: {
@@ -64,8 +65,27 @@ UserSchema.statics.findByToken = function (token) {
     '_id': decoded._id,
     'tokens.token': token,
     'tokens.access': 'auth'
-  })
+  });
 };
+
+// UserSchema.pre('save', function (next) {
+//   var user = this;
+//
+//   if(user.isModified('password')) {
+//     // user.password
+//     bcrypt.genSalt(10, (err, salt) => {
+//       bcrypt.hash(user.password, salt, (err, hash) => {
+//         user.password = hash;
+//         next();
+//       });
+//     });
+//
+//     // user.password = hash;
+//     // next();
+//   } else {
+//     next();
+//   }
+// });
 
 //creating  a new model for a user collection in my local database
 var User = mongoose.model('User', UserSchema);
