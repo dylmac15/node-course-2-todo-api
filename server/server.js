908b62dc7f5fe7098c5b7b5b2660aa2a7371710e
Dylan <dylan@Dylans-MacBook-Pro.local>
2018-07-28 12:35:14 -0400

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -29,9 +29,6 @@ app.post('/todos', (req, res) => { //POST request
 
 //POST /users
 
-
-
-
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
@@ -127,6 +124,22 @@ app.post('/users', (req, res) => {
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
+
+//POST /users/login {email, password}
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+
 
 if(!module.parent){
   app.listen(port, () => {
